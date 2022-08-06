13ecf3ad0b0c266b9a917a323f8e8d727cfb1b08
ShanChenqi <chenqishan337@gmail.com>
2022-08-06 23:16:08 +0800

    apdate author_message

--- grimoire_elk_gitee/raw/gitee.py
+++ grimoire_elk_gitee/raw/gitee.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2022 Haiming Lin, Yehu Wang, Chenqi Shan
+# Copyright (C) 2021-2022 Haiming Lin, Yehui Wang, Chenqi Shan
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
